7849b172035a7b5efb01d60a77406d9c8e9b8b14
demetrio <dem.iaria@gmail.com>
2024-06-22 13:03:50 +0200

    feat: Update login layout to remove unused code

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -7,7 +7,9 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
     const router = useRouter()
     const handleClick = () => {
         router.push('/forgotten-password');
-        router.replace('/forgotten-password'); //Navigate to the provided href. Replaces the current history entry.
+        //router.replace('/forgotten-password'); //Navigate to the provided href. Replaces the current history entry.
+        //router.back(); //Navigate back in history
+        //router.forward(); //Navigate forward in history
     }
 
     return (
