b55b208c2c50039bc6db76a5b9006788de749c5e
demetrio <dem.iaria@gmail.com>
2024-06-22 18:27:39 +0200

    feat: Add reset button to Error component

--- src/app/(auth)/error.tsx
+++ src/app/(auth)/error.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-export default function Error({ error }: { error: Error }) {
+export default function Error({ error, reset }: { error: Error, reset: () => void }) {
     return (
         <div>
             <h1>{error.message}</h1>
+            <button onClick={reset}>Try again</button>
         </div>
     );
 }
